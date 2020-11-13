b7f790afb5c29c7a99fedaa09cdbded3bdafd741
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-11-13 08:58:11 +0100

    Set the right repo location

--- _adhesive.py
+++ _adhesive.py
@@ -3,7 +3,7 @@ import germanium_py_exe  # type: ignore
 
 germanium_py_exe.pipeline(
     {
-        "repo": "git@github.com:bmustiata/GermaniumLibrary.git",
+        "repo": "git@github.com:germaniumhq/robotframework-germaniumlibrary.git",
         "binaries": {
             "name": "Python 3.8 on Linux x64",
             "platform": "python:3.8",
