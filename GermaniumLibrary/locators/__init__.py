1a109112dc71a23c94ac78ed73a2227792c1f962
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-11-13 08:56:24 +0100

    Updated documentation

--- GermaniumLibrary/locators/__init__.py
+++ GermaniumLibrary/locators/__init__.py
@@ -28,6 +28,7 @@ from germanium.static import (
     Text,
     Alert,
     Window,
+    S,
 )
 from robot.api.deco import keyword
 
@@ -56,22 +57,22 @@ class GermaniumLocators:
 
     @keyword
     def click(self, selector: str):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         click(s)
 
     @keyword
     def double_click(self, selector: str):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         double_click(s)
 
     @keyword
     def hover(self, selector: str):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         hover(s)
 
     @keyword
     def right_click(self, selector: str):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         right_click(s)
 
     @keyword
@@ -84,78 +85,98 @@ class GermaniumLocators:
 
     @keyword
     def type_keys_in(self, selector, keys):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         type_keys(keys, s)
 
     @keyword
     def drag_and_drop(self, start_selector, end_selector):
-        start = self._create_selector(start_selector)
-        end = self._create_selector(end_selector)
+        start = self._resolve_selector(start_selector)
+        end = self._resolve_selector(end_selector)
         drag_and_drop(from_selector=start, to_selector=end)
 
     @keyword
     def select_by_index(self, selector, index):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         select(s, index=eval(index, globals(), locals()))
 
     @keyword
     def select_by_text(self, selector, text):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         select(s, text=eval(text, globals(), locals()))
 
     @keyword
     def select_by_value(self, selector, value):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         select(s, value=eval(value, globals(), locals()))
 
     @keyword
     def deselect_by_index(self, selector, index):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         deselect(s, index=eval(index, globals(), locals()))
 
     @keyword
     def deselect_by_text(self, selector, text):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         deselect(s, text=eval(text, globals(), locals()))
 
     @keyword
     def deselect_by_value(self, selector, value):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         deselect(s, value=eval(value, globals(), locals()))
 
     @keyword
     def select_file(self, selector, file_path):
-        s = self._create_selector(selector)
+        s = self._resolve_selector(selector)
         select_file(s, file_path=file_path)
 
-    def _create_selector(self, selector):
-        try:
-            s = eval(selector, self.selector_globals(), locals())
+    @keyword
+    def assert_exists(self, selector):
+        s = self._create_selector(selector)
+        assert s.exists(), f"No elements matching selector {selector} could be found."
 
+    @keyword
+    def assert_missing(self, selector):
+        s = self._create_selector(selector)
+        assert s.not_exists(), f"Selector {selector} matched elements"
+
+    def _resolve_selector(self, selector):
+        s = self._create_selector(selector)
+
+        try:
             return waited(s)
         except Exception as e:
             raise Exception(f"Unable to resolve {selector}", e)
 
+    def _create_selector(self, selector):
+        try:
+            s = S(eval(selector, self.selector_globals(), locals()))
+
+        except Exception as e:
+            raise Exception(f"Unable to create selector {selector}", e)
+
+        return s
+
     def selector_globals(self):
-        result = dict(globals())
+        extended_globals = dict(globals())
 
-        result["Css"] = Css
-        result["XPath"] = XPath
-        result["JsSelector"] = JsSelector
-        result["AnyOfSelector"] = AnyOfSelector
-        result["Button"] = Button
-        result["Element"] = Element
-        result["Input"] = Input
-        result["CheckBox"] = CheckBox
-        result["Image"] = Image
-        result["InputText"] = InputText
-        result["InputFile"] = InputFile
-        result["Link"] = Link
-        result["Text"] = Text
-        result["Alert"] = Alert
-        result["Window"] = Window
+        extended_globals["S"] = S
+        extended_globals["Css"] = Css
+        extended_globals["XPath"] = XPath
+        extended_globals["JsSelector"] = JsSelector
+        extended_globals["AnyOfSelector"] = AnyOfSelector
+        extended_globals["Button"] = Button
+        extended_globals["Element"] = Element
+        extended_globals["Input"] = Input
+        extended_globals["CheckBox"] = CheckBox
+        extended_globals["Image"] = Image
+        extended_globals["InputText"] = InputText
+        extended_globals["InputFile"] = InputFile
+        extended_globals["Link"] = Link
+        extended_globals["Text"] = Text
+        extended_globals["Alert"] = Alert
+        extended_globals["Window"] = Window
 
-        return result
+        return extended_globals
 
     @staticmethod
     def noop():
